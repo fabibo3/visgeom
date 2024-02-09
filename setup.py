0f0b89afab57e106ab7ce6806e7daf6767654a67
Fabi Bongratz <fabi.bongratz@tum.de>
2024-02-09 11:32:35 +0100

    Make installable with setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+setup(
+    name='visgeom',
+    version='0.1',
+    author='Fabian Bongratz',
+    packages = find_packages(),
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'visgeom=visgeom.visgeom:main'
+        ]
+    }
+)
